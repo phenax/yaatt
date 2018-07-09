c4e563ae0099b23f5dc3bb53822ca328bf84811b
Akshay Nair <phenax5@gmail.com>
2018-07-09 11:29:45 +0530

    Adds tests and refactors createClass

--- src/utils/create-class.js
+++ src/utils/create-class.js
@@ -2,30 +2,34 @@
 
 import { curry, compose, identity } from 'ramda';
 
-export const initializeClassMethods = curry((methods: Object, obj: Object) => {
-	obj.__methods = methods;
-	Object.keys(methods).forEach(name =>
-		obj[name] = methods[name](obj));
+type Methods = { [key: string]: any => Function };
+type ClassDefinition = {
+	constructor: Function,
+	...Methods,
+};
+
+export const initializeClassMethods = curry((methods: Methods, obj: Object) => {
+	Object.keys(methods).forEach(name => {
+		obj[name] = (...args) =>  methods[name](obj)(...args);
+	});
 	return obj;
 });
 
-export const attachUtility = (constructor: Function, methods: Object) => (Factory: Function) => {
-	Factory.constructor = constructor;
-	Factory.methods = methods;
-	Factory.extend = ({ constructor: childConstructor, ...childMethods }) => 
-		createClass({
-			constructor: compose(
-				childConstructor,
-				Factory.constructor,
-			),
-			...Factory.methods,
-			...childMethods,
-		});
+export const attachUtility = (constructor: Function, methods: Methods) => (Factory: Function) => {
+	Factory.$$ = { ...methods };
+	Factory.$$.constructor = constructor;
+	Factory.extend =
+		({ constructor: childConstructor = identity, ...childMethods }) => 
+			createClass({
+				constructor: compose( childConstructor, constructor ),
+				...methods,
+				...childMethods,
+			});
 	return Factory;
 };
 
 export const createClass =
-	({ constructor = identity, ...methods }: Object) =>
+	({ constructor = identity, ...methods }: ClassDefinition) =>
 		attachUtility(constructor, methods)(
 			compose(
 				initializeClassMethods(methods),
--- test/utils/create-class.test.js
+++ test/utils/create-class.test.js
@@ -1,12 +1,78 @@
 
-// import { throwError, toParams } from '../src/utils';
+import { createClass } from '../../src/utils/create-class';
+
+const isOdd = x => x % 2;
 
 describe('utils', () => {
 
-	describe('throwError', () => {
+	describe('create class', () => {
 
-		it('should throw e if given an object', () => {
+		it('should execute the constructor', () => {
+			const OddList = createClass({
+				constructor: list => ({
+					list: list.filter(isOdd)
+				}),
+			});
 
+			const odds = OddList([ 1, 2, 3, 4, 5, 6 ]);
+
+			expect(odds.list).toEqual([ 1, 3, 5 ]);
+		});
+
+		it('should pass instance to methods', () => {
+			const OddList = createClass({
+				constructor: list => ({
+					list: list.filter(isOdd)
+				}),
+				first: self => () => self.list[0],
+			});
+
+			const odds = OddList([ 2, 3, 4, 5, 6 ]);
+
+			expect(odds.first()).toBe(3);
+		});
+
+		it('should pass destructured values to methods even after changes', () => {
+			const OddList = createClass({
+				constructor: list => ({
+					list: list.filter(isOdd)
+				}),
+				concat: self => newItems =>
+					self.list = self.list.concat(newItems),
+				get: ({ list }) => index =>
+					list[index],
+			});
+
+			const odds = OddList([ 2, 3, 4, 5, 6 ]);
+
+			odds.concat([ 99, 100 ]);
+
+			expect(odds.get(2)).toBe(99);
+			expect(odds.get(3)).toBe(100);
+		});
+
+		it('should inherit methods from parent class with .extend static method', () => {
+
+			const NumberList = createClass({
+				constructor: list => ({ list }),
+				concat: self => newItems =>
+					self.list = self.list.concat(newItems),
+				get: ({ list }) => index => list[index],
+			});
+
+			const OddList = NumberList.extend({
+				constructor: ({ list }) => ({
+					list: list.filter(isOdd)
+				}),
+				concat: self => newItems =>
+					self.list = self.list.concat(newItems.filter(isOdd)),
+			});
+
+			const numbers = NumberList([ 2, 3, 4, 5, 6 ]);
+			const odds = OddList([ 2, 3, 4, 5, 6 ]);
+
+			expect(numbers.get(0)).toBe(2);
+			expect(odds.get(0)).toBe(3);
 		});
 	});
 });
