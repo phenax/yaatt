f4c313406996e3fd5a9c617a7ce1fc9e4c320ad0
Akshay Nair <phenax5@gmail.com>
2018-07-11 20:52:09 +0530

    Fixes linter config for react

--- packages/yaatt-docs/src/index.js
+++ packages/yaatt-docs/src/index.js
@@ -1,15 +1,1 @@
-
 export * from './docs-generator';
-import { renderTestSuite } from './docs-generator';
-
-console.log(renderTestSuite({
-	label: 'Httpbin Get call',
-	request: {
-		url: 'http://httpbin.org/get',
-		method: 'get',
-	},
-	tests: {
-		'should have name set to Waluigi': { request: { params: { name: 'Waluigi' } } },
-		'should have custom header set to Yep': { },
-	},
-}));
